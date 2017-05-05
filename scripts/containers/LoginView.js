a3c1f7b1b45a0daa8d87705ec843ba313c2591b9
尹文楷 <yinwk@servyou.com.cn>
2017-05-05 17:38:03 +0800

    login page building

--- scripts/containers/LoginView.js
+++ scripts/containers/LoginView.js
@@ -2,6 +2,7 @@
  * Created by yinwk on 2017/5/5.
  */
 import React from "react";
+import {Input, Button} from "antd";
 
 class LoginView extends React.Component {
     constructor(props) {
@@ -14,6 +15,26 @@ class LoginView extends React.Component {
             <main className="main-container">
                 <section className="login-section">
                     <div className="container-shadow">
+                    </div>
+                    <div className="login-container">
+                        <h1 className="login-title">登录</h1>
+                        <span className="login-spin">login</span>
+                        <p> </p>
+                        <div className="input-email-container">
+                            <Input
+                                className="login-input-email"
+                                placeholder="用户名"
+                                size="large"
+                                maxLength="15"
+                            />
+                            <Button
+                                type="primary"
+                                size="large"
+                                className="login-button-next"
+                            >
+                                下一步
+                            </Button>
+                        </div>
                     </div>
                 </section>
                 <section className="register-section">
